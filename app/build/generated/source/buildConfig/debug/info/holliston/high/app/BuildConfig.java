8eacd2a42c940caa7cce5b50a91a2e6f2dd72e31
TReeve <moosereeve@gmail.com>
2017-03-02 08:20:07 -0500

    Updates to remove signing keys

--- app/build/generated/source/buildConfig/debug/info/holliston/high/app/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/info/holliston/high/app/BuildConfig.java
@@ -8,6 +8,11 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "info.holliston.high.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20;
-  public static final String VERSION_NAME = "";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "2.2";
+  // Fields from build type: debug
+  public static final String KEY_ALIAS = "ReeveHHSApp";
+  public static final String KEY_PW = "fenway1";
+  public static final String STORE_FILE = "../../ReeveHHS.jks";
+  public static final String STORE_PW = "fenway1";
 }
